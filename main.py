97b230f5bf1f9483f64ad721df7b3bd94384341f
Baray Ann <annbaray66@gmail.com>
2024-10-04 12:36:38 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-a=list(map(int, input("Введите числа списка: ").split()))
+a=map(int, input("Введите числа списка: ").split())
 print("Ваш список: ", a)
 b=[i**2 for i in a]
 print("Квадраты чисел вашего списка:", b)
